9d9534e51f49b339ec3d913d6c6c3df20bfc2a0a
Charles Pina <charlespina@gmail.com>
2013-06-02 10:43:15 -0700

    removed unnecessary ng dep, added a comment about reloadOnSearch

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
-angular.module('photoapp', ['flickrService', 'photoappDirectives', 'ng']).
+angular.module('photoapp', ['flickrService', 'photoappDirectives']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider
             .when('/', { 
                 templateUrl: 'partials/list.html', 
                 controller: 'ListController',
-                reloadOnSearch: false
+                reloadOnSearch: false // prevents ?foo=bar url's from reloading controllers
             })
             .when('/set/:setId', {
                 templateUrl: 'partials/list-set.html',
